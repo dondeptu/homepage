0e1038af3947bb21799ccf78276a635814eb1254
Donata Deptuła <dondeptu@gmail.com>
2023-05-29 23:12:35 +0200

    refactoring toogleImageSize, init functions

--- js/script.js
+++ js/script.js
@@ -2,15 +2,14 @@
     const welcome = () => { console.log("Hello Friends!"); };
 
     const toggleImageSize = () => {
-        let image = document.querySelector(".js-headerImage");
+        const image = document.querySelector(".js-headerImage");
 
         image.classList.toggle("header__image--resizeImage");
         resizeImageButton.innerText = image.classList.contains("header__image--resizeImage") ? "Pomniejsz" : "Powiększ";
     }
 
     const init = () => {
-        let resizeImageButton = document.querySelector(".js-resizeImageButton");
-
+        const resizeImageButton = document.querySelector(".js-resizeImageButton");
         resizeImageButton.addEventListener("click", toggleImageSize);
 
         welcome();
